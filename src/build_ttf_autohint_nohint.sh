16bdab07d59e0b9c3bd2a274f4432366faebf59a
Marc Foley <m.foley.88@gmail.com>
2017-01-23 10:45:34 +0000

    fonts | src: added

---
+++ src/build_ttf_autohint_nohint.sh
@@ -0,0 +1,4 @@
+ttfautohint -d ./Rubik-Regular_vttauto.ttf ../fonts/Rubik-Regular_no_hint.ttf
+ttfautohint -x 0 ../fonts/Rubik-Regular_no_hint.ttf ../fonts/Rubik-Regular_ttfauto_no_x.ttf
+
+echo "fonts generated, check ../fonts/"
